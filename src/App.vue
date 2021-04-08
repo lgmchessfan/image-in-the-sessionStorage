cd8b11053f9b28ad51ac6c34b1fbd77b17b95ffd
lgmchessfan <leonardo.gmx@gmail.com>
2021-04-08 16:48:35 -0500

    commit inicial

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="file" accept="image/*;capture=camera" @input="signalChange" />
+    <br />
+    <button @click="muestralo">Presiona para mostrar imagen!!!</button>
+    <button @click="borraSESS">Borrar session</button>
+    <button @click="borraIMG">Borrar Imagen</button>
+    <hr />
+    <img id="output" width="250px" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {};
+  },
+
+  methods: {
+    muestralo: function() {
+      if (sessionStorage.imagen) {
+        var output = document.getElementById("output");
+        var file = JSON.parse(sessionStorage.imagen);
+        output.src = file.dataURL;
+      } else {
+        alert("No hay imagen cargada!");
+      }
+    },
+    borraSESS() {
+      sessionStorage.clear();
+    },
+    borraIMG() {
+      var output = document.getElementById("output");
+      output.src = null;
+    },
+    signalChange: function(evt) {
+      var element = evt.srcElement;
+      var file = element.files[0],
+        reader = new FileReader();
+
+      reader.onload = (function() {
+        return function(e) {
+          file.dataURL = e.target.result;
+          //console.log(JSON.stringify(file));
+
+          // var output = document.getElementById("output");
+          if (typeof Storage !== "undefined") {
+            sessionStorage.imagen = JSON.stringify(file);
+          }
+          // output.src = file.dataURL;
+        };
+      })(file);
+      reader.readAsDataURL(file);
+    },
+  },
+};
 </script>
 
 <style>
